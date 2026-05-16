5641202778ce0a58b9cc86050086fc60027292d0
Jakub Warszewski <s30979@pjwstk.edu.pl>
2026-05-17 00:22:50 +0200

    AppDbContext

--- ComputerManager/ComputerManager.API/Data/AppDbContext.cs
+++ ComputerManager/ComputerManager.API/Data/AppDbContext.cs
@@ -15,4 +15,11 @@ public class AppDbContext : DbContext
     public DbSet<Component> Components { get; set; }
     public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
     public DbSet<ComponentType> ComponentTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PcComponent>()
+            .HasKey(pc => new { pc.PcId, pc.ComponentCode });
+    }
+    
 }
