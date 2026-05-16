78bcac5f268fb9f1b7cc2a83aa362a6f4b6c73ab
Jakub Warszewski <s30979@pjwstk.edu.pl>
2026-05-16 23:28:22 +0200

    AppDbContext

---
+++ ComputerManager/ComputerManager.API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ComputerManager.API.Entities;
+
+namespace ComputerManager.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Pc> Pcs { get; set; }
+    public DbSet<PcComponent> PcComponents { get; set; }
+    public DbSet<Component> Components { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+}
