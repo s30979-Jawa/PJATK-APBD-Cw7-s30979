d0bdfba537cf133300689b3669d5aeb2bb3d2202
Jakub Warszewski <s30979@pjwstk.edu.pl>
2026-05-17 01:34:28 +0200

    seed

--- ComputerManager/ComputerManager.API/Data/AppDbContext.cs
+++ ComputerManager/ComputerManager.API/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ComputerManager.API.Entities;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace ComputerManager.API.Data;
 
@@ -20,6 +21,41 @@ public class AppDbContext : DbContext
     {
         modelBuilder.Entity<PcComponent>()
             .HasKey(pc => new { pc.PcId, pc.ComponentCode });
+
+        //comp type
+        modelBuilder.Entity<ComponentType>().HasData(
+            new ComponentType { Id = 1, Abbreviation = "CPU", Name = "Processor" },
+            new ComponentType { Id = 2, Abbreviation = "GPU", Name = "Graphics Card" },
+            new ComponentType { Id = 3, Abbreviation = "RAM", Name = "Memory" }
+        );
+
+        //comp manu
+        modelBuilder.Entity<ComponentManufacturer>().HasData(
+            new ComponentManufacturer {Id = 1, Abbreviation = "AMD", FullName = "Advanced Micro Devices", FoundationDate = new DateOnly(1969, 5, 1)},
+            new ComponentManufacturer {Id = 2, Abbreviation = "NV", FullName = "NVIDIA Corporation", FoundationDate = new DateOnly(1993, 4, 5)},
+            new ComponentManufacturer {Id = 3, Abbreviation = "COR", FullName = "Corsair Gaming Inc.", FoundationDate = new DateOnly(1994, 1, 1)}
+        );
+        
+        //pc
+        modelBuilder.Entity<Pc>().HasData(
+            new Pc {Id = 1, Name = "Komputer1", Weight = 50, Warranty = 5, CreatedAt = new DateTime(2025, 5, 5, 9, 0, 0), Stock = 1},
+            new Pc {Id = 2, Name = "Stary Komputer", Weight = 700, Warranty = 1, CreatedAt = new DateTime(1999, 2, 12, 7, 3, 12), Stock = 12},
+            new Pc {Id = 3, Name = "Nowy Komputer", Weight = 900, Warranty = 20, CreatedAt = new DateTime(2014, 8, 12, 17, 32, 45), Stock = 5}
+        );
+
+        //comp
+        modelBuilder.Entity<Component>().HasData(
+            new Component {Code = "GPU0000001", Name = "GjePeUu", Description = "To jest GPU", ComponentManufacturerId = 2, ComponentTypeId = 2},
+            new Component {Code = "CPU9999999", Name = "Super Duper Processor", Description = "To jest CPU", ComponentManufacturerId = 1, ComponentTypeId = 1},
+            new Component {Code = "RAM1234567", Name = "Osiem Bajtow", Description = "To jest RAM", ComponentManufacturerId = 3, ComponentTypeId = 3}
+        );
+
+        //PcComp
+        modelBuilder.Entity<PcComponent>().HasData(
+            new PcComponent {Amount = 30, ComponentCode = "RAM1234567", PcId = 3},
+            new PcComponent {Amount = 20, ComponentCode = "CPU9999999", PcId = 2},
+            new PcComponent {Amount = 10, ComponentCode = "GPU0000001", PcId = 1}
+        );
     }
     
 }
---
+++ ComputerManager/ComputerManager.API/Migrations/20260516232644_SeedData.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ComputerManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ComponentManufacturers",
+                columns: new[] { "Id", "Abbreviation", "FoundationDate", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "AMD", new DateOnly(1969, 5, 1), "Advanced Micro Devices" },
+                    { 2, "NV", new DateOnly(1993, 4, 5), "NVIDIA Corporation" },
+                    { 3, "COR", new DateOnly(1994, 1, 1), "Corsair Gaming Inc." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ComponentTypes",
+                columns: new[] { "Id", "Abbreviation", "Name" },
+                values: new object[,]
+                {
+                    { 1, "CPU", "Processor" },
+                    { 2, "GPU", "Graphics Card" },
+                    { 3, "RAM", "Memory" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pcs",
+                columns: new[] { "Id", "CreatedAt", "Name", "Stock", "Warranty", "Weight" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 5, 5, 9, 0, 0, 0, DateTimeKind.Unspecified), "Komputer1", 1, 5, 50f },
+                    { 2, new DateTime(1999, 2, 12, 7, 3, 12, 0, DateTimeKind.Unspecified), "Stary Komputer", 12, 1, 700f },
+                    { 3, new DateTime(2014, 8, 12, 17, 32, 45, 0, DateTimeKind.Unspecified), "Nowy Komputer", 5, 20, 900f }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Components",
+                columns: new[] { "Code", "ComponentManufacturerId", "ComponentTypeId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { "CPU9999999", 1, 1, "To jest CPU", "Super Duper Processor" },
+                    { "GPU0000001", 2, 2, "To jest GPU", "GjePeUu" },
+                    { "RAM1234567", 3, 3, "To jest RAM", "Osiem Bajtow" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PcComponents",
+                columns: new[] { "ComponentCode", "PcId", "Amount" },
+                values: new object[,]
+                {
+                    { "GPU0000001", 1, 10 },
+                    { "CPU9999999", 2, 20 },
+                    { "RAM1234567", 3, 30 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PcComponents",
+                keyColumns: new[] { "ComponentCode", "PcId" },
+                keyValues: new object[] { "GPU0000001", 1 });
+
+            migrationBuilder.DeleteData(
+                table: "PcComponents",
+                keyColumns: new[] { "ComponentCode", "PcId" },
+                keyValues: new object[] { "CPU9999999", 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PcComponents",
+                keyColumns: new[] { "ComponentCode", "PcId" },
+                keyValues: new object[] { "RAM1234567", 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Components",
+                keyColumn: "Code",
+                keyValue: "CPU9999999");
+
+            migrationBuilder.DeleteData(
+                table: "Components",
+                keyColumn: "Code",
+                keyValue: "GPU0000001");
+
+            migrationBuilder.DeleteData(
+                table: "Components",
+                keyColumn: "Code",
+                keyValue: "RAM1234567");
+
+            migrationBuilder.DeleteData(
+                table: "Pcs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Pcs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Pcs",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentManufacturers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentManufacturers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentManufacturers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ComponentTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
