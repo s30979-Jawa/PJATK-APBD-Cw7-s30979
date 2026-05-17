f7ddd9376adff4229313142b70d02a9d44a5ba53
Jakub Warszewski <s30979@pjwstk.edu.pl>
2026-05-17 02:44:01 +0200

    service 1

---
+++ ComputerManager/ComputerManager.API/Services/PcService.cs
@@ -0,0 +1,50 @@
+﻿using ComputerManager.API.Data;
+using ComputerManager.API.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerManager.API.Services;
+
+public class PcService : IPcService
+{
+    private readonly AppDbContext _context;
+    
+    public PcService(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<PcGetAllDto>> GetAllAsync()
+    {
+        return await _context.Pcs
+            .Select(pc => new PcGetAllDto
+            {
+                Id = pc.Id,
+                Name = pc.Name,
+                Weight = pc.Weight,
+                Warranty = pc.Warranty,
+                CreatedAt = pc.CreatedAt,
+                Stock = pc.Stock
+            })
+            .ToListAsync();
+    }
+
+    public Task<PcGetByIdDto?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PcGetAllDto> CreateAsync(PcCreateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(int id, PcUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
